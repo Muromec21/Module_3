6e2160c8accf0e506b4ccfe32a7190b5588af36d
Muromec21 <Muromec2606@gmail.com>
2024-12-16 15:57:01 +0300

    Add files via upload

--- Module_3_2.py
+++ Module_3_2.py
@@ -1,35 +1,30 @@
-def send_email(message, recipient, *, sender = 'university.help@gmail.com'):
-    list_ = ['@']
-    list_2 = ['.com', '.ru', '.net']
-    for i in list_:
-        if i not in sender:
-            print('Невозможно отправить письмо с адреса <', sender, '> на адрес <', recipient, '>')
-            break
-        else:
-            for j in list_2:
-                if j in sender:
-                    break
-            else:
-                print('Невозможно отправить письмо с адреса <', sender, '> на адрес <', recipient, '>')
-                break
+def send_email(message, recipient, *, sender='university.help@gmail.com'):
+    address = '.com', '.ru', '.net'
+    if '@' not in sender:
+        print('Невозможно отправить письмо с адреса <', sender, '> на адрес <', recipient, '>')
+    else:
 
-        for i in list_:
-            if i not in recipient:
-                print('Невозможно отправить письмо с адреса <', sender, '> на адрес <', recipient, '>')
-                break
-            else:
-                for j in list_2:
-                    if j in recipient:
-                        break
-                else:
-                    print('Невозможно отправить письмо с адреса <', sender, '> на адрес <', recipient, '>')
-        if sender == recipient:
-            print('Нельзя отправить письмо самому себе!')
+        if not sender.endswith(address):
+            print('Невозможно отправить письмо с адреса <', sender, '> на адрес <', recipient, '>')
         else:
-            if sender == 'university.help@gmail.com':
-                print('Письмо успешно отправлено с адреса <', sender, '> на адрес <', recipient, '>.')
+
+            if '@' not in recipient:
+                print('Невозможно отправить письмо с адреса <', sender, '> на адрес <', recipient, '>')
             else:
-                print('НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса <', sender, '> на адрес <', recipient, '>.')
+
+                if not recipient.endswith(address):
+                    print('Невозможно отправить письмо с адреса <', sender, '> на адрес <', recipient, '>')
+                else:
+
+                    if sender == recipient:
+                        print('Нельзя отправить письмо самому себе!')
+                    else:
+                        if sender == 'university.help@gmail.com':
+                            print('Письмо успешно отправлено с адреса <', sender, '> на адрес <', recipient, '>.')
+                        else:
+                            print('НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса <', sender, '> на адрес <',
+                                  recipient, '>.')
+
 
 send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
 send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
