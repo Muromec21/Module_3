048aea29fa19b9517161f22959d5e16e5bc4cb44
Muromec21 <Muromec2606@gmail.com>
2024-12-18 03:39:43 +0300

    Add files via upload

---
+++ Module_3_3.py
@@ -0,0 +1,10 @@
+def print_params(a = 1, b = 'строка',c = True):
+    print(a, b, c)
+print_params(b = 25)
+print_params(c = [1, 2, 3])
+values_list = [2, 'Список', False]
+values_dict = {'a': 3, 'b': 'Словарь', 'c': True}
+print_params(*values_list)
+print_params(**values_dict)
+values_list_2 = [1, False]
+print_params(*values_list_2, 42)
